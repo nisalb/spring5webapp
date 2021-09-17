ed19a95f330b5c9a6a31c9fa658817f6a24f0c6d
Nisal Bandara <nisal.bandara@outlook.com>
2021-09-17 15:43:29 +0530

    Initialize Spring Web App with Undertow
    Signed-off-by: Nisal Bandara <nisal.bandara@outlook.com>

---
+++ src/test/java/me/nisalb/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nisalb.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
